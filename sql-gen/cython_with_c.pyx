e23d4c486653bfc613ccf0039609da57b1b73b99
Vahan Martirosyan <vahan@zidder.HP>
2019-10-22 20:56:16 +0400

    Generalize cython programs
    Test Plan: none
    
    Reviewers: arsen
    
    Reviewed By: arsen
    
    Differential Revision: http://phabricator.hilearn.io/D2658

--- sql-gen/cython_with_c.pyx
+++ sql-gen/cython_with_c.pyx
@@ -10,15 +10,16 @@ ctypedef np.float64_t dtype_t
 def convert_to_string(np.ndarray[dtype_t, ndim=1] a):
     cdef char *ret = <char *> malloc(9008)
     cdef char *p = ret
+    cdef int n = a.shape[0]
     ret[0]='\0'
     p = mystrcat(p, "ARRAY[")
-    for i in range(999):
+    for i in range(n):
         if a[i] is None:
-            p = mystrcat(p, "'null',")
+            p = mystrcat(p, "'null'")
         else:
-            p = mystrcat(p, str.encode(f'{a[i]:.4f},'))
-    if a[999] is None:
-        p = mystrcat(p, "'null',")
-    else:
-        p = mystrcat(p, str.encode(f'{a[999]:.4f}]'))
+            p = mystrcat(p, str.encode(f'{a[i]:.4f}'))
+        if i == n - 1:
+            p = mystrcat(p, "]")
+        else:
+            p = mystrcat(p, ",")
     return ret.decode()
--- sql-gen/cython_with_cpp.pyx
+++ sql-gen/cython_with_cpp.pyx
@@ -3,13 +3,13 @@ from libcpp.string cimport string
 
 def convert_to_string(np.ndarray[np.float64_t, ndim=1] a):
     cdef string ret = "ARRAY["
-    for i in range(999):
+    cdef int n = a.shape[0]
+    for i in range(n):
         if a[i] is None:
-            ret.append("'null',")
+            ret.append("'null'")
         else:
-            ret.append(<string>str.encode(f'{a[i]:.4f},'))
-    if a[999] is None:
-        ret.append("'null']")
-    else:
-        ret.append(<string>str.encode(f'{a[999]:.4f}]'))
-    return ret
+            ret.append(<string>str.encode(f'{a[i]:.4f}'))
+        if i == n - 1:
+            ret.append("]")
+        else:
+            ret.append(",")
